24c9aa8b4e6cc19817e175a9369f749afc57307c
Sourabh Sen <sourabhsen201313@gmail.com>
2022-06-11 16:33:56 +0530

    DB changes
    DB changes

--- src/config/database/database.js
+++ src/config/database/database.js
@@ -1,10 +1,16 @@
 const { mongoose } = require("mongoose")
-mongoose.connect("mongodb://localhost:27017/ilqna_dev_11_06_2022", (err) => {
-    if(err) 
-        console.log("Error from Database connectivity: ", err)
-    else 
-        console.log("MongoDb Connected.");
-})
+
+exports.connectDB = async () => {
+    await mongoose.connect("mongodb://localhost:27017/ilqna_dev_11_06_2022", {
+        
+    }).then(() => {
+        console.log("MongoDb Connected!")
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+
 
 
 module.exports = mongoose
